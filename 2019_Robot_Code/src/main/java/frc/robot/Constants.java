2806da20b4a6b411c80343086820f6980f2c937e
Jakob Misbach <jakobmisbach8@gmail.com>
2019-01-10 21:32:01 -0500

    Worked on drive code

---
+++ 2019_Robot_Code/src/main/java/frc/robot/Constants.java
@@ -0,0 +1,25 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+public class Constants {
+
+    //Motor Controller and Drive Train Constants
+    public static VictorSP portMaster;
+    public static VictorSP portSlave;
+    public static VictorSP starboardMaster;
+    public static VictorSP starboardSlave;
+
+    public static final int portMasterPort = 0;
+    public static final int portSlavePort = 1;
+    public static final int starboardMasterPort = 2;
+    public static final int starboardSlavePort = 3;
+
+    public static DifferentialDrive driveTrain;
+
+    //Joystick Constants
+    public static final int joystickPort = 0;
+    public static Joystick driveJoystick;
+}
--- 2019_Robot_Code/src/main/java/frc/robot/Robot.java
+++ 2019_Robot_Code/src/main/java/frc/robot/Robot.java
@@ -1,34 +1,51 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 public class Robot extends TimedRobot {
 
-  @Override
-  public void robotInit() {
-    
-  }
+    @Override
+    public void robotInit() {
+        //Motor Controller Definition
+        Constants.portMaster = new VictorSP(Constants.portMasterPort);
+        Constants.portSlave = new VictorSP(Constants.portSlavePort);
+        Constants.starboardMaster = new VictorSP(Constants.starboardMasterPort);
+        Constants.starboardSlave = new VictorSP(Constants.starboardSlavePort);
 
-  @Override
-  public void robotPeriodic() {
-  }
+        SpeedControllerGroup portMotorGroup = new SpeedControllerGroup(Constants.portMaster, Constants.portSlave);
+        SpeedControllerGroup starboardMotorGroup = new SpeedControllerGroup(Constants.starboardMaster, Constants.starboardSlave);
 
-  @Override
-  public void autonomousInit() {
+        Constants.driveTrain = new DifferentialDrive(portMotorGroup, starboardMotorGroup);
 
-  }
+        //Joystick Controller Definition
+        Constants.driveJoystick = new Joystick(Constants.joystickPort);
+    }
 
-  @Override
-  public void autonomousPeriodic() {
+    @Override
+    public void robotPeriodic() {
+        
+    }
 
-  }
+    @Override
+    public void autonomousInit() {
 
-  @Override
-  public void teleopPeriodic() {
-  }
+    }
 
-  @Override
-  public void testPeriodic() {
-    
-  }
+    @Override
+    public void autonomousPeriodic() {
+
+    }
+
+    @Override
+    public void teleopPeriodic() {
+    }
+
+    @Override
+    public void testPeriodic() {
+
+    }
 }
