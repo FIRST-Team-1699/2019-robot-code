af21cc302ad3357d888e1a45d798f40665c97378
Jakob Misbach <jakobmisbach8@gmail.com>
2019-02-08 20:47:11 -0500

    Fixed merge issues

--- 2019_Robot_Code/src/main/java/frc/robot/subsystems/DriveBase.java
+++ 2019_Robot_Code/src/main/java/frc/robot/subsystems/DriveBase.java
@@ -1,8 +1,5 @@
 package frc.robot.subsystems;
 
-
-public class DriveBase extends Subsystem{
-
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
@@ -15,7 +12,7 @@ public class DriveBase extends Subsystem {
     private final SpeedControllerGroup portGroup, starGroup;
     private PeriodicIO periodicIO = new PeriodicIO();
 
-    private DriveBase(){
+    private DriveBase() {
         portMaster = new VictorSP(0);
         portSlave = new VictorSP(1);
         starMaster = new VictorSP(4);
@@ -27,8 +24,8 @@ public class DriveBase extends Subsystem {
         drive = new DifferentialDrive(portGroup, starGroup);
     }
 
-    public static DriveBase getInstance(){
-        if(instance == null){
+    public static DriveBase getInstance() {
+        if (instance == null) {
             instance = new DriveBase();
         }
         return instance;
@@ -40,7 +37,7 @@ public class DriveBase extends Subsystem {
     }
 
     @Override
-    public synchronized void writePeriodicOutputs(){
+    public synchronized void writePeriodicOutputs() {
         drive.arcadeDrive(periodicIO.rotateDemand, periodicIO.forwardDemand);
     }
 
@@ -60,9 +57,7 @@ public class DriveBase extends Subsystem {
 
     }
 
-}
-
-    public synchronized void setOpenLoop(double forwardDemand, double rotateDemand){
+    public synchronized void setOpenLoop(double forwardDemand, double rotateDemand) {
         //TODO Change state
         periodicIO.forwardDemand = forwardDemand;
         periodicIO.rotateDemand = rotateDemand;
@@ -75,4 +70,7 @@ public class DriveBase extends Subsystem {
         //Outputs
         public double rotateDemand;
         public double forwardDemand;
+    }
+
+}
 
