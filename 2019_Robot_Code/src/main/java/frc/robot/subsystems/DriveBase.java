20cea409f311fc127e1733ce1df82db0d6095f34
Seth Crampton <seth.crampton@gmail.com>
2019-02-08 20:31:05 -0500

    seths changes

---
+++ 2019_Robot_Code/src/main/java/frc/robot/subsystems/DriveBase.java
@@ -0,0 +1,21 @@
+package frc.robot.subsystems;
+
+public class DriveBase extends Subsystem{
+
+    @Override
+    public boolean checkSystem() {
+        return false;
+    }
+
+    @Override
+    public void outputTelemetry() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+
+}
